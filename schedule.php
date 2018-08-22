ef5a79e0c137b4cc0d6dde6a49300b2cf0fb9fe8
sifat <sifatistiak@gmail.com>
2018-08-22 13:13:25 +0600

    v1.1(home_develop_v1.0)

--- schedule.php
+++ schedule.php
@@ -5,3 +5,12 @@
  * Date: 22-Aug-18
  * Time: 1:04 PM
  */
+require_once './api/connection.php';
+date_default_timezone_set('Asia/Dhaka');
+$week_day = date("l");
+$sql = "SELECT name FROM schedule WHERE day ='$week_day';";
+$res = $conn->query($sql);
+$row = $res->fetch_assoc();
+$_SESSION['bazar_name']=$row['name'];
+echo "Today Bazzar will be done by : ".$_SESSION['bazar_name'];
+?>
