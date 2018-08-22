6e61770c54bab89097ccd5699d8fa4228e14194d
sifat <sifatistiak@gmail.com>
2018-08-22 13:29:08 +0600

    v1.1(home_develop_v1.0)

--- schedule.php
+++ schedule.php
@@ -13,4 +13,14 @@ $res = $conn->query($sql);
 $row = $res->fetch_assoc();
 $_SESSION['bazar_name']=$row['name'];
 echo "Today Bazzar will be done by : ".$_SESSION['bazar_name'];
+$sql1 = "SELECT name FROM schedule;";
+$res1 = $conn->query($sql1);
+
+echo "<table>"; // start a table tag in the HTML
+
+while($row1 = $res1->fetch_assoc()){   //Creates a loop to loop through results
+    echo "<tr><td>" . $row['day'] . "</td><td>" . $row['name'] . "</td></tr>";  //$row['index'] the index here is a field name
+}
+
+echo "</table>";
 ?>
